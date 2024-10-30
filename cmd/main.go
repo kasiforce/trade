04aa65936ff43ab983be2339a0838d388cce2fba
sysu <chenjy787@mail2.sysu.edu.cn>
2024-10-30 23:18:43 +0800

    程序启动(version1)

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	_ "fmt"
+	"github.com/CocaineCong/gin-mall/routes"
+	conf "github.com/kasiforce/trade/config"
+	"github.com/kasiforce/trade/pkg/util"
+	"github.com/kasiforce/trade/repository/db/dao"
+)
 
+func main() {
+	loading()
+	r := routes.NewRouter()
+	_ = r.Run(conf.Config.System.HttpPort)
+	fmt.Println("启动成功...")
+}
+
+func loading() {
+	conf.InitConfig() //配置文件初始化
+	util.InitLog()    //日志文件初始化
+	dao.InitMySQL()   //数据库初始化
 }
