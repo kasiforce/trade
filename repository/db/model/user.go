2dc6b5110f254a067e63ac3afae22ff7fa7948e8
sysu <chenjy787@mail2.sysu.edu.cn>
2024-10-31 22:02:04 +0800

    定义User模型，与数据库users表交互

--- repository/db/model/user.go
+++ repository/db/model/user.go
@@ -1,5 +1,17 @@
-package main
+package model
 
-func main() {
+type User struct {
+	userID     int    `gorm:"primaryKey;autoIncrement;column:userID"`
+	userName   string `gorm:"type:varchar(30);unique;not null;column:userName"`
+	passwords  string `gorm:"type:varchar(30);not null;column:passwords"`
+	schoolID   int    `gorm:"not null;column:schoolID"`
+	picture    string `gorm:"type:varchar(256);column:picture"`
+	tel        string `gorm:"type:varchar(20);column:tel"`
+	mail       string `gorm:"type:varchar(40);unique;not null;column:mail"`
+	gender     int    `gorm:"type:tinyint;column:gender"`
+	userStatus int    `gorm:"type:tinyint;column:userStatus;default:1"`
+}
 
+func (User) TableName() string {
+	return "users"
 }
