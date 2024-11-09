9f7ce072d70a4e572b2281875a7800170e95d47e
sysu <chenjy787@mail2.sysu.edu.cn>
2024-11-09 14:14:33 +0800

    定义用户信息响应、用户添加/修改请求结构体

--- types/user.go
+++ types/user.go
@@ -1,5 +1,31 @@
-package main
+package types
 
-func main() {
+type UserInfoResp struct {
+	UserID   int    `json:"userID"`
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+	SchoolID int    `json:"schoolID"`
+	Picture  string `json:"picture"`
+	Tel      string `json:"tel"`
+	Mail     string `json:"mail"`
+	Gender   int    `json:"gender"`
+	Status   int    `json:"status"`
+}
 
+type UserAddReq struct {
+	UserName string `form:"userName" json:"userName"`
+	Password string `form:"password" json:"password"`
+	SchoolID int    `form:"schoolID" json:"schoolID"`
+	Mail     string `form:"mail" json:"mail"`
+}
+
+type UserInfoUpdateReq struct {
+	UserID   int    `url:"userID" json:"userID"`
+	UserName string `form:"userName" json:"userName"`
+	SchoolID int    `form:"schoolID" json:"schoolID"`
+	Picture  string `form:"picture" json:"picture"`
+	Tel      string `form:"tel" json:"tel"`
+	Mail     string `form:"mail" json:"mail"`
+	Gender   int    `form:"gender" json:"gender"`
+	Status   int    `form:"status" json:"status"`
 }
