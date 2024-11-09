31108d0be9b1f1b0cc7290b97077d8cba15413a7
sysu <chenjy787@mail2.sysu.edu.cn>
2024-11-09 15:24:24 +0800

    定义 url和对应处理接口

--- routes/routes.go
+++ routes/routes.go
@@ -1,5 +1,30 @@
-package main
+package routes
 
-func main() {
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/kasiforce/trade/api"
+	"github.com/kasiforce/trade/middleware"
+	"net/http"
+)
 
+func NewRouter() *gin.Engine {
+	router := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+	router.Use(middleware.Cors())
+	router.StaticFS("/static", http.Dir("./static"))
+	v1 := router.Group("/trade")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, "success")
+		})
+		v1.GET("/admin/user/:id", api.ShowUserInfoHandler())
+		v1.GET("/admin/user", api.ShowAllUserHandler())
+		v1.POST("admin/user", api.AddUserHandler())
+		v1.PUT("/admin/user/:id", api.UpdateUserHandler())
+		v1.GET("/home/category", api.ShowCategoryHandler())
+	}
+	return router
 }
