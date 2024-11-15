c00d4c06bf121a325e7cb25f2621acc620654c33
cghdjvjg <1251356634@qq.com>
2024-11-15 22:17:55 +0800

    定义数据库

--- repository/db/model/refund_records.go
+++ repository/db/model/refund_records.go
@@ -9,7 +9,7 @@ type RefundRecord struct {
 	RefundAgreedTime  time.Time `gorm:"type:datetime;not null;column:refundAgreedTime"`
 	RefundShippedTime time.Time `gorm:"type:datetime;column:refundShippedTime"`
 	RefundArrivalTime time.Time `gorm:"type:datetime;column:refundArrivalTime"`
-	TrackingNumber    string    `gorm:"type:varchar(10);column:trackingNumber"`
+	TrackingNumber    string    `gorm:"type:varchar(100);column:trackingNumber"`
 }
 
 func (RefundRecord) TableName() string {
