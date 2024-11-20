af6476f1d657b4d686df5f4cc7f1d7f2f2adf502
sysu <chenjy787@mail2.sysu.edu.cn>
2024-11-20 09:59:59 +0800

    添加数据库操作

--- repository/db/dao/category.go
+++ repository/db/dao/category.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"context"
 	"github.com/kasiforce/trade/repository/db/model"
+	"github.com/kasiforce/trade/types"
 	"gorm.io/gorm"
 )
 
@@ -18,7 +19,33 @@ func NewCategory(c context.Context) *Category {
 	return &Category{NewDBClient(c)}
 }
 
-func (c *Category) FindAll() (ca []*model.Category, err error) {
+func (c *Category) FindAll(req types.ShowCategoryReq) (ca []*model.Category, err error) {
+	db := c.DB
+	//db = db.Table("category").Select("category.*")
+	if req.SearchQuery != "" {
+		db = db.Where("categoryName LIKE ?", "%"+req.SearchQuery+"%")
+	}
+	db = db.Limit(req.PageSize).Offset((req.PageNum - 1) * req.PageSize)
+	err = db.Find(&ca).Error
+	return
+}
+
+func (c *Category) ShowCategory() (ca []*model.Category, err error) {
 	err = c.DB.Model(&model.Category{}).Find(&ca).Error
+	return
+}
+
+func (c *Category) AddCategory(category *model.Category) (err error) {
+	err = c.DB.Model(&model.Category{}).Create(&category).Error
+	return
+}
+
+func (c *Category) UpdateCategory(id int, category *model.Category) (err error) {
+	err = c.DB.Model(&model.Category{}).Where("categoryID = ?", id).Updates(&category).Error
+	return
+}
+
+func (c *Category) DeleteCategory(id int) (err error) {
+	err = c.DB.Model(&model.Category{}).Delete(&model.Category{}, id).Error
 	return
 }
