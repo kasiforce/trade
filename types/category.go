16656d0f3ee47b2d3d620d54aa887114766fac49
sysu <chenjy787@mail2.sysu.edu.cn>
2024-11-20 09:52:27 +0800

    添加类别请求体

--- types/category.go
+++ types/category.go
@@ -1,7 +1,18 @@
 package types
 
-type CategoryResp struct {
+type CategoryInfo struct {
 	CategoryID   int    `json:"categoryID"`
 	CategoryName string `json:"categoryName"`
 	Descriptions string `json:"description"`
+}
+type CategoryList struct {
+	Categories []CategoryInfo `json:"categoryList"`
+	Total      int            `json:"total"`
+	PageNum    int            `json:"pageNum"`
+}
+
+type ShowCategoryReq struct {
+	SearchQuery string `form:"searchQuery" json:"searchQuery"`
+	PageNum     int    `form:"pageNum" json:"pageNum"`
+	PageSize    int    `form:"pageSize" json:"pageSize"`
 }
