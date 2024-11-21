62ac2a551c31ae301188081a3d5c42933318121e
cghdjvjg <1251356634@qq.com>
2024-11-21 16:57:45 +0800

    定义数据库

--- repository/db/model/report.go
+++ repository/db/model/report.go
@@ -8,6 +8,8 @@ type Report struct {
 	GoodsID     int       `gorm:"not null;column:goodsID"`
 	Reason      string    `gorm:"type:text;not null;column:reason"`
 	CreatedTime time.Time `gorm:"type:datetime;not null;column:createdTime"`
+	User        User      `gorm:"foreignKey:UserID;references:UserID"`
+	Goods       Goods     `gorm:"foreignKey:GoodsID;references:GoodsID"`
 }
 
 func (Report) TableName() string {
