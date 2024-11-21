41dac625df02b878feaf2fcd5dbcd92b5e15052a
cghdjvjg <1251356634@qq.com>
2024-11-21 19:15:23 +0800

    更新管理员数据库操作

--- repository/db/dao/admin.go
+++ repository/db/dao/admin.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"context"
 	"github.com/kasiforce/trade/repository/db/model"
+	"github.com/kasiforce/trade/types"
 	"gorm.io/gorm"
 )
 
@@ -18,27 +19,48 @@ func NewAdmin(ctx context.Context) *Admin {
 	return &Admin{NewDBClient(ctx)}
 }
 
-func (a *Admin) FindAll() (admins []*model.Admin, err error) {
-	err = a.DB.Model(&model.Admin{}).Find(&admins).Error
+func (admin *Admin) FindAll(req types.ShowAdminReq) (admins []model.Admin, err error) {
+	db := admin.DB
+	query := db.Table("admins").Select("admins.*")
+	if req.SearchQuery != "" {
+		query = query.Where("admins.adminName LIKE ?", "%"+req.SearchQuery+"%")
+	}
+	query = query.Limit(req.PageSize).Offset((req.PageNum - 1) * req.PageSize)
+	err = query.Find(&admins).Error
 	return
 }
 
-func (a *Admin) FindByID(id int) (admin *model.Admin, err error) {
-	err = a.DB.Model(&model.Admin{}).Where("adminID = ?", id).First(&admin).Error
+func (admin *Admin) FindByID(id int) (a *model.Admin, err error) {
+	err = admin.DB.Model(&model.Admin{}).Where("adminID = ?", id).First(&a).Error
 	return
 }
 
-func (a *Admin) CreateAdmin(admin *model.Admin) (err error) {
-	err = a.DB.Model(&model.Admin{}).Create(&admin).Error
+func (admin *Admin) FindByName(name string) (exist bool, err error) {
+	var cnt int64
+	err = admin.DB.Model(&model.Admin{}).Where("adminName = ?", name).Count(&cnt).Error
+	if cnt == 0 {
+		return false, err
+	}
+	return true, err
+}
+
+func (admin *Admin) CreateAdmin(data map[string]interface{}) (err error) {
+	err = admin.DB.Model(&model.Admin{}).Create(data).Error
 	return
 }
 
-func (a *Admin) UpdateAdmin(id int, admin *model.Admin) (err error) {
-	err = a.DB.Model(&model.Admin{}).Where("adminID = ?", id).Updates(admin).Error
+func (admin *Admin) UpdateAdmin(id int, data map[string]interface{}) (err error) {
+	err = admin.DB.Model(&model.Admin{}).Where("adminID = ?", id).Updates(data).Error
 	return
 }
 
-func (a *Admin) DeleteAdmin(id int) (err error) {
-	err = a.DB.Model(&model.Admin{}).Where("adminID = ?", id).Delete(&model.Admin{}).Error
+func (admin *Admin) DeleteAdmin(id int) (err error) {
+	err = admin.DB.Model(&model.Admin{}).Delete(&model.Admin{}, id).Error
+	return
+}
+
+// CheckMail 登录时检查邮箱是否存在，若存在查出用户名、id、密码
+func (admin *Admin) CheckMail(mail string) (a *model.Admin, err error) {
+	err = admin.DB.Model(&model.Admin{}).Where("mail = ?", mail).First(&a).Error
 	return
 }
