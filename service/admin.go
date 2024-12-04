51e4e9f03373640475fda2885d60558b7de0cff1
lipj <2117123431@qq.com>
2024-12-04 16:20:25 +0800

    service/admin.go中的依赖修改

--- service/admin.go
+++ service/admin.go
@@ -3,14 +3,7 @@ package service
 import (
 	"context"
 	"errors"
-	"sync"
-
-	"github.com/cghdjvjg/trade/pkg/ctl"
-	"github.com/cghdjvjg/trade/pkg/util"
-	"github.com/cghdjvjg/trade/repository/db/dao"
-	"github.com/cghdjvjg/trade/types"
 	"github.com/gin-gonic/gin"
-	"github.com/kasiforce/trade/middleware"
 	"github.com/kasiforce/trade/pkg/util"
 	"github.com/kasiforce/trade/repository/db/dao"
 	"github.com/kasiforce/trade/types"
