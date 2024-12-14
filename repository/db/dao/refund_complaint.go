4ec88340c5090170175a4a6662957a5fc49593bc
cghdjvjg <39019400+cghdjvjg@users.noreply.github.com>
2024-12-14 18:19:42 +0800

    查询所有待退货商品

--- repository/db/dao/refund_complaint.go
+++ repository/db/dao/refund_complaint.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"context"
 	"github.com/kasiforce/trade/repository/db/model"
+	"github.com/kasiforce/trade/types"
 	"gorm.io/gorm"
 )
 
@@ -18,8 +19,24 @@ func NewRefundComplaint(ctx context.Context) *RefundComplaint {
 	return &RefundComplaint{NewDBClient(ctx)}
 }
 
-func (rc *RefundComplaint) FindAll() (refundComplaints []*model.RefundComplaint, err error) {
-	err = rc.DB.Model(&model.RefundComplaint{}).Find(&refundComplaints).Error
+func (rc *RefundComplaint) CountAll() (total int64, err error) {
+	err = rc.DB.Model(&model.RefundComplaint{}).Count(&total).Error
+	return
+}
+
+func (rc *RefundComplaint) FindAll(req types.ShowRefundReq) (refundComplaints []*model.RefundComplaint, err error) {
+	query := rc.DB.Model(&model.RefundComplaint{}).
+		Joins("JOIN trade_records t ON t.tradeID = refund_complaint.tradeID").
+		Joins("JOIN users seller ON seller.userID = t.sellerID").
+		Joins("JOIN users buyer ON buyer.userID = t.buyerID").
+		Joins("JOIN goods ON goods.goodsID = t.goodsID").
+		//Joins("JOIN refund_complaint rc ON rc.tradeID = refund_records.tradeID").
+		Select("refund_complaint.tradeID, refund_complaint.cTime, t.orderTime, t.payTime, t.shippingTime, t.turnoverTime, seller.userName AS sellerName, buyer.userName AS buyerName, goods.goodsName AS goodsName, goods.price AS price, t.shippingCost AS shippingCost, refund_complaint.buyerReason AS buyerReason, refund_complaint.sellerReason AS sellerReason, refund_complaint.cStatus AS cStatus, seller.userID AS sellerID, buyer.userID AS buyerID")
+	if req.SearchQuery != "" {
+		query = query.Where("refund_complaint.tradeID = ?", req.SearchQuery)
+	}
+	query = query.Limit(req.PageSize).Offset((req.PageNum - 1) * req.PageSize)
+	err = query.Debug().Find(&refundComplaints).Error
 	return
 }
 
--- repository/db/model/refund_complaint.go
+++ repository/db/model/refund_complaint.go
@@ -11,6 +11,18 @@ type RefundComplaint struct {
 	CTime        time.Time `gorm:"type:datetime;not null;column:cTime"`
 	CStatus      int       `gorm:"type:tinyint;not null;default:0;column:cStatus"`
 	SellerReason string    `gorm:"type:text;column:sellerReason"`
+	OrderTime    time.Time `gorm:"type:datetime;not null;column:orderTime"`
+	PayTime      time.Time `gorm:"type:datetime;column:payTime"`
+	ShippingTime time.Time `gorm:"type:datetime;column:shippingTime"`
+	TurnoverTime time.Time `gorm:"type:datetime;column:turnoverTime"`
+	SellerName   string    `gorm:"not null;column:sellerName"`
+	BuyerName    string    `gorm:"not null;column:buyerName"`
+	GoodsName    string    `gorm:"not null;column:goodsName"`
+	SellerID     int       `gorm:"not null;column:sellerID"`
+	BuyerID      int       `gorm:"not null;column:buyerID"`
+	GoodsID      int       `gorm:"not null;column:goodsID"`
+	ShippingCost float64   `gorm:"type:decimal(10,2);not null;check:turnoverAmount >= 0;column:turnoverAmount"`
+	Price        float64   `gorm:"type:decimal(10,2);not null;check:price >= 0;column:price"`
 }
 
 func (RefundComplaint) TableName() string {
